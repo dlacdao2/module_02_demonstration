e9513fc36d2ecfb68938caee468358f340c72f52
Donwin Lacdao <dlacdao2@academic.RRC.CA>
2023-09-12 09:38:34 -0500

    Initial commit: Documented Project.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Module 02 Demonstration
+
+## Description
+- Introdution to Python
+- Documentation
+- Variables
+- Printing
+
+## Author
+Donwin Lacdao
---
+++ module_02.py
@@ -0,0 +1,13 @@
+"""
+Description: Module 02 Demonstration
+Author: Donwin Lacdao
+Date: September 12, 2023
+Usage: Run using the play button in the IDE (Integrated Development Environment). 
+"""
+
+# This is an inline comment.
+
+'''
+This is s multi-line comment.
+It spans over multiple lines.
+'''
