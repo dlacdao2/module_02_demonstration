5f52cd91a1006ef21987b268bd1620a39b5c0d1f
Donwin Lacdao <dlacdao2@academic.RRC.CA>
2023-09-14 09:22:29 -0500

    Experimented with lists. Created collections.py file.

--- collections.py
+++ collections.py
@@ -29,3 +29,37 @@ print(daily_step_count)
 popped = daily_step_count.pop()
 print(popped)
 print(daily_step_count)
+
+
+#[10343, 9385, 7029, 10931, 9999]
+daily_step_count.sort()
+print(daily_step_count)
+
+daily_step_count.reverse()
+print(daily_step_count)
+
+ascending = sorted(daily_step_count)
+print(ascending)
+print(daily_step_count)
+
+print(daily_step_count[2])
+
+# slicing : splits a list
+# start: default is position 0 
+# stop: default at the end 
+# step: default 1 step 
+red_river_string = "RRC Polytechnic"
+red_river_list = list(red_river_string)
+
+print(red_river_list)
+# ['R', 'R', 'C', ' ', 'P', 'o', 'l', 'y', 't', 'e', 'c', 'h', 'n', 'i', 'c']
+print(red_river_list[2: 12: 2]) # C P l t c 
+
+print(red_river_list[: 10: 1])
+
+print(red_river_list[5: : 3]) # o t h c
+
+print(red_river_list[-1])
+
+#RRC Polytechnic
+print(red_river_list[-1: -5: -1])
